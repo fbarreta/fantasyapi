5940f03602060022656d460583cb4bcd9a8c98b5
fbarreta <fbarreta@gmail.com>
2017-09-21 16:00:50 -0300

    Update conn.js

--- api/dao/conn.js
+++ api/dao/conn.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
   connectionLimit : 100,
   host     : 'localhost',
   user     : 'root',
-  password : 'masterkey',
+  password : 'xxxx',
   database : 'adm',
   debug    :  false
 });
